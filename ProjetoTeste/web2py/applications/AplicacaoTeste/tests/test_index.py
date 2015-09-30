0f6ce489d90de638aed70d376145a21eed1ec994
juliana <j2padilha@yahoo.com>
2015-09-30 07:07:35 -0700

    Update test_index.py

--- ProjetoTeste/web2py/applications/AplicacaoTeste/tests/test_index.py
+++ ProjetoTeste/web2py/applications/AplicacaoTeste/tests/test_index.py
@@ -1,6 +1,11 @@
 from unittest import TestCase
 
 # Importa o controlador que sera testado
+import os,sys
+
+print '>>>>',os.path.dirname(__file__)
+print '>>>>',sys.path
+
 import default
 
 __author__ = 'TecSUS-3'
